5665f3cb7d5ec93fbe75d87baad02b0df5e1aa2c
Subil H <subilhashi@email.com>
2026-05-18 15:59:05 +0530

    Csp added

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,7 @@ const nextConfig = {
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
           { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
+          { key: "Content-Security-Policy", value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' blob: data: https:; font-src 'self' data: https:; connect-src 'self' https:; frame-src 'self' https:;" },
         ],
       },
     ];
